aca7fe393b9c0a81673b39dfc0a87681993b5cbf
Rose Weixel <rose.weixel@gmail.com>
2015-01-29 15:37:12 -0500

    finish specs

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -1,6 +1,9 @@
 describe "git repository" do
   it "has a .git file" do
-    expect(File).to exist("./lab-inside-lab/.git") 
-    #./lab-inside-lab/.git
+    expect(File).to exist("./lab-inside-lab/.git")
+  end
+
+  it "has a readme" do
+    expect(File).to exist("./lab-inside-lab/README.md")
   end
 end
